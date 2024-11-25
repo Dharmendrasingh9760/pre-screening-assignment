d60988b2256a574768902babac546369ced12fb4
Dharmendrasingh9760 <152598648+Dharmendrasingh9760@users.noreply.github.com>
2024-11-25 15:18:58 +0530

    Update database.py

--- assignment/backend/database.py
+++ assignment/backend/database.py
@@ -1,6 +1,17 @@
-from databases import Database
+# database.py
+import psycopg2
 
-DATABASE_URL = "postgresql://admin:admin9760@localhost:5432/db-1"
+def get_db_connection():
+    conn = psycopg2.connect(
+        dbname="db-1",
+        user="admin",
+        password="admin9760",
+        host="postgres",  # The service name of PostgreSQL in Docker Compose
+        port=5432
+    )
+    return conn
 
-# Async database instance
-database = Database(DATABASE_URL)
+def release_db_connection(conn):
+    """Release the database connection by closing it."""
+    if conn:
+        conn.close()
