cc7d46dd28744ef2988f23cfe3ba190337133abb
Dharmendrasingh9760 <152598648+Dharmendrasingh9760@users.noreply.github.com>
2024-11-25 16:02:53 +0530

    Update database.py

--- assignment/backend/database.py
+++ assignment/backend/database.py
@@ -2,7 +2,7 @@ import sqlalchemy as _sql
 import sqlalchemy.ext.declarative as _declarative
 import sqlalchemy.orm as _orm
 
-DATABASE_URL = "postgresql://myuser:password@localhost/fastapi_database"
+DATABASE_URL = "postgresql://admin:admin9760@localhost/db-1"
 
 engine = _sql.create_engine(DATABASE_URL)
 
